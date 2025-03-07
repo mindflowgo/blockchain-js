5f3e82c2c01e359d1b5e0328ee1cf6a4c716c57f
Filipe Laborde <the@mindflow.network>
2025-03-07 13:59:40 -0500

    Added ledger block-depth for users.

--- ledger.js
+++ ledger.js
@@ -86,13 +86,15 @@ async function main(){
             }
 
             // arrange alphabettically and display
-            debug('dim',`   = Name =${' '.repeat(14)} = TX Balance =${' '.repeat(11)} = Block Balance =`)
+            debug('dim',`   = Name =${' '.repeat(14)} = Unconfirmed =${' '.repeat(6)} = On-Chain Balance =  = Depth =`)
             wallets.filter( w => w.name ).sort((a, b) =>  a.name.localeCompare(b.name)).forEach( i=>{
                 const name = i.name.length>19 ? i.name.substring(0,17)+'...' : i.name 
                 if( !(name.length===0 && name === '_') && i.tx.balance>0 ){
                     let seqInfo = i.tx?.seq > 0 || i.onChain?.seq > 0 ? `${i.tx.seq},${i.onChain.seq}` : ''
                     seqInfo = !seqInfo ? ':' : '/' + seqInfo + ':'
-                    debug('dim',`   - ${name}${seqInfo}${' '.repeat(20-(seqInfo.length+name.length))} $ ${i.tx.balance || '0'} ${' '.repeat(22-i.tx.balance.toString().length)} ${i.tx.balance === i.onChain.balance ? '  "' : `$ `+i.onChain.balance} ${' '.repeat(20-(i.tx.balance === i.onChain.balance ? '  "' : `$ `+i.onChain.balance).toString().length)} ${i.note || ''}`)
+                    debug('dim',`   - ${name}${seqInfo}${' '.repeat(20-(seqInfo.length+name.length))} $ ${i.tx.balance || '0'} `
+                              + `${' '.repeat(18-i.tx.balance.toString().length)} ${i.tx.balance === i.onChain.balance ? '  "' : `$ `+i.onChain.balance} ${' '.repeat(20-(i.tx.balance === i.onChain.balance ? '  "' : `$ `+i.onChain.balance).toString().length)} ${i.note || ''}`
+                              + `${' '.repeat(3-i.depth.toString().length)}${i.depth}`)
                 }
                 })
             break
